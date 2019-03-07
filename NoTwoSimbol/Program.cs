d91edc4876b68b87f8fbf0f42c1d10474472907c
Липская Юлия <yuliya.santotsckay@yandex.ru>
2019-03-07 19:58:47 +0300

    $ git commit

--- NoTwoSimbol/Program.cs
+++ NoTwoSimbol/Program.cs
@@ -4,12 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NoTwoSimbol
+
+namespace task1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // string s = "access";
+            NoTwoEqualSimpol();
+            //  NoTwoEqualSimpol(string s);
+
         }
+
+
+        static void NoTwoEqualSimpol()
+        {
+            string s = "mammochka";
+            // char[] arr = s.ToCharArray();
+            //Console.WriteLine("Enter somesing");
+            //s = Console.ReadLine();
+
+            if ((s.Length <= 1) || (s.Length == 0))
+                Console.WriteLine("String must be longer");
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    continue;
+
+                    //  b = s[i] + s[i + 1];
+                    // Console.WriteLine(b);
+                }
+                else
+                {
+                    Console.Write(s[i]);
+                    Console.Write(s[i + 1]);
+                    Console.Write(",");
+                }
+              
+
+            }
+
+            Console.Read();
+        }
+
+
+
     }
 }
+        
+    
+
+
+
+
